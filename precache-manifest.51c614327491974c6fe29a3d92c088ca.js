870ed164a04310ab87b3b7ac8ae25bac7df7b736
Eirik Vullum <evullum@gmail.com>
2019-03-27 16:05:36 +0100

    Updates

---
+++ precache-manifest.51c614327491974c6fe29a3d92c088ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployment-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f4c874c79d5deb0b78d",
+    "url": "/deployment-test/static/js/runtime~main.848ed5c4.js"
+  },
+  {
+    "revision": "8c090a22ad52fbb64e62",
+    "url": "/deployment-test/static/js/main.052e1314.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/deployment-test/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "8c090a22ad52fbb64e62",
+    "url": "/deployment-test/static/css/main.cde5840e.chunk.css"
+  },
+  {
+    "revision": "5aad6cfe69e516816f3556eeab260f41",
+    "url": "/deployment-test/index.html"
+  }
+];
